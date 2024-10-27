551dc346721e77c1ea85138b4717be90556f5a98
thirdygayares <gayaresthird@gmail.com>
2024-10-28 01:42:09 +0800

    feat: Add page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'firepage';
+  pages: number[] = [0]; // Initial page
+
+  addPage() {
+    this.pages.push(this.pages.length);
+  }
 }
